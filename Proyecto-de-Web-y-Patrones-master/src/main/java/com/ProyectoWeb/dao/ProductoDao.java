2a3ed1eebf9f5a0a254d7cf7bfa246969da9ba9a
Mateo Rincón Gonzalez <127810785+MrShadowx69@users.noreply.github.com>
2024-03-26 11:57:03 -0600

    Add files via upload

---
+++ Proyecto-de-Web-y-Patrones-master/src/main/java/com/ProyectoWeb/dao/ProductoDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ProyectoWeb.dao;
+
+import com.ProyectoWeb.domain.Producto;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author secog
+ */
+public interface ProductoDao extends JpaRepository<Producto, Long> {
+
+    List<Producto> findByPrecioBetweenOrderByPrecio(double precioInf, double precioSup);
+
+}
